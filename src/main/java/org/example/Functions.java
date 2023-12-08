a53752df91d5bb204587069a7dc6593366ee511d
Oleg <r96_23@list.ru>
2023-12-08 22:06:23 +0500

    add function min, max

--- src/main/java/org/example/Functions.java
+++ src/main/java/org/example/Functions.java
@@ -2,14 +2,11 @@ package org.example;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.time.chrono.ChronoLocalDate;
-import java.time.temporal.ChronoField;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Functions {
 
-    private static java.time.chrono.ChronoLocalDate ChronoLocalDate;
 
     public static void getExpensesList(List<Patient> patients) { //forEach
         patients
@@ -33,6 +30,7 @@ public class Functions {
                 .stream()
                 .noneMatch(p -> Period.between(p.getBirthDate(), currentDate).getYears() == 100);
     }
+
     public static Boolean checkAnyAgeMan(List<Patient> patients) { //anyMatch
         LocalDate currentDate =LocalDate.now();
         return patients
@@ -40,15 +38,45 @@ public class Functions {
                 .allMatch(p -> Period.between(p.getBirthDate(), currentDate).getYears() == 100);
     }
 
-    public static Comparator<Patient> comp = Comparator
-            .comparing(Patient::getBirthDate);
-
-    /*public static Map<LocalDate, String> getMapFromList(List<Patient> patients) {
+    public static Map<LocalDate, String> getMapFromList(List<Patient> patients) { //collect
 
         Map<LocalDate, String> localDateStringMap;
         return localDateStringMap =
                 patients
                 .stream()
                 .collect(Collectors.toMap(p -> p.getBirthDate(), p -> p.getFio()));
-    }*/
+    }
+
+    public static Comparator<Patient> comp = (p1, p2) -> { //comparator
+        int result;
+        result = p1.getExpenses()
+                .stream()
+                .reduce((acc, cur) -> acc + cur)
+                .get()
+                .compareTo(p2.getExpenses()
+                        .stream()
+                        .reduce((acc, cur) -> acc + cur)
+                        .get());
+        if (result == 0) {
+            if (p1.getBirthDate().equals(p2.getBirthDate())) return 0;
+            else if (p1.getBirthDate().isBefore(p2.getBirthDate())) {
+                return 1;
+            }
+            else return -1;
+        } else return result;
+    };
+
+    public static Patient getMaxExpPatient(List<Patient> patients) { //max
+        return patients
+                .stream()
+                .max(comp)
+                .get();
+    }
+
+    public static Patient getMinExpPatient(List<Patient> patients) { //min
+        return  patients
+                .stream()
+                .min(comp)
+                .get();
+    }
 }
